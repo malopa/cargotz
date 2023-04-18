43d1b5518fb2e3ba8bd121f4797f423226a6df04
malopa <malopalusajo@gmail.com>
2023-04-18 15:47:21 +0300

    slider

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-$('selector').flickity()
 
-alert("hellow")
+
+  var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+  autoPlay: true
+});
